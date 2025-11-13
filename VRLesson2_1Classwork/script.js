0e387b39df1705655580da12d87d84e0192f2ba0
radracer249 <paull62@nycstudents.net>
2025-11-13 15:38:13 +0000

    blah

--- VRLesson2_1Classwork/script.js
+++ VRLesson2_1Classwork/script.js
@@ -1,10 +1,20 @@
 window.addEventListener("DOMContentLoaded",function() {
-  let scene = document.querySelector("a-scene");
+  //Challenge 1: Create a variable scene for the <a-scene> component in the HTML
 
-  let box = document.createElement("a-box");
 
-  box.setAttribute("position",{x: -2, y: 2, z: -1});
+  //Challenge 2: Create a variable for <a-dodecahedron> and create it.
 
-  scene.append(box);
+  
+  //Challenge 3: Change the <a-dodecahedron>'s position, radius and color
+
+
+  //Challenge 4: Add the <a-dodecahedron> to the scene
+
+
+  /* Challenge Bonus
+  1) Use a for loop to create a lot of <a-dodecahedron> 
+  2) Change each <a-dodecahedron>'s position to a random x, y, and z location
+  3) Change each <a-dodecahedron>'s to a random color.  Hint: Use rgb( ) and string interpolation 
+  */
   
 })
